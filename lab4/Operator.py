07e890ae7173740e47743e621d2d244e4b4d479e
Alex Samaroo <alexsamaroo32@gmail.com>
2023-03-28 19:23:36 -0400

    Created Map File and finished Mine functions

--- lab4/Operator.py
+++ lab4/Operator.py
@@ -7,9 +7,12 @@
 #
 #-----------------------------------------------------
 
-from fastapi import FastAPI
-from typing import List
+import requests
+import json
+from fastapi import FastAPI, HTTPException
+from typing import List, Tuple
 from pydantic import BaseModel
+from random import randint
 
 app = FastAPI()
 
@@ -20,7 +23,7 @@ app = FastAPI()
 
 #Todo: Create objects for Map, Rover, and Mines
 class Map(BaseModel):
-    data: List[List][int]
+    data: List[List[int]]
 
 class Rover(BaseModel):
     id: int
@@ -31,6 +34,15 @@ class Rover(BaseModel):
 
 #db to store all rovers
 rovers_db = {}
+
+class Mine(BaseModel):
+    xpos: int
+    ypos: int
+    isDefused: bool = False
+    serialNum: int
+    id: int
+
+mines_db = {}
 
 #--------------------------------------------------
 #
@@ -63,36 +75,45 @@ def updateField(map_data: Map):
 #GET: Retrieve list of mines with their coordinates and serial numbers
 @app.get('/mines')
 def retrieveListMines():
-    #do something
-    return {'Output': 'This is a test for the Get Mines.'}
+    return list(mines_db.values())
 
 #GET: Retrieve specified mine with serial number and coordinates
 #Throws exception if Mine not found (404 error)
-@app.get('/mines/:id')
-def retrieveMine():
-    #do something
-    return {'Output': 'This is a test for the Get Mine with ID.'}
+@app.get('/mines/{mine_id}')
+def retrieveMine(mine_id: int):
+    if mine_id not in mines_db:
+        raise HTTPException(status_code=404, detail="Mine not found")
+    return mines_db[mine_id]
 
 #DELETE: Delete a mine specified by their ID
 #Throws 404 error if Mine not found
-@app.delete('/mines/:id')
-def deleteMine():
-    #do something
-    return {'Output': 'This is a test for the Delete Mine.'}
+@app.delete('/mines/{mine_id}')
+def deleteMine(mine_id: int):
+    if mine_id not in mines_db:
+        raise HTTPException(status_code=404, detail="Mine not found")
+    del mines_db[mine_id]
+    return {"message": f"Mine {mine_id} has been successfully deleted"}
 
 #POST: Create a new mine
 @app.post('/mines')
-def addMine():
-    #add the mine
-    return {'Output': 'This is a test for the Add Mine.'}
+def addMine(mine_data: Mine):
+    mine_id = len(mines_db) + 1
+    mine = Mine(id=mine_id, **mine_data.dict())
+    mine.serialNum = randint(100, 999)
+    mines_db[mine_id] = mine
+    return mine
 
 #PUT: Update a mine
 #404 error if Mine not found
-@app.put('/mines/:id')
-def updateMine():
-    #do something
-    return {'Output': 'This is a test for the Put Mine.'}
-
+@app.put('/mines/{mine_id}')
+def updateMine(mine_id: int):
+    if mine_id not in mines_db:
+        raise HTTPException(status_code=404, detail="Mine not found")
+    elif mines_db[mine_id].isDefused == True:
+        raise HTTPException(status_code=405, detail="Mine has been defused. CTs Win!")
+    mines_db[mine_id].xpos = randint(1,5)
+    mines_db[mine_id].ypos = randint(1,5)
+    return {"message": f"Mine {mine_id} updated successfully."}
 #--------------------------------------------------
 #
 #                 Rover functions
@@ -106,7 +127,7 @@ def retrieveListRovers():
 
 #GET: Retrieve rover specified by ID number
 #Throw exception if Rover not found (404 error)
-@app.get('/rovers/:id')
+@app.get('/rovers/{rover_id}')
 def retrieveRover(rover_id: int):
     if rover_id not in rovers_db:
         raise HTTPException(status_code=404, detail="Rover not found")
@@ -114,9 +135,11 @@ def retrieveRover(rover_id: int):
 
 #POST: Create new rover
 @app.post('/rovers')
-def addRover():
-    #add the mine
-    return {'Output': 'This is a test for the Add Rover.'}
+def addRover(rover_data: Rover):
+    rover_id = len(rovers_db) + 1
+    rover = Rover(id=rover_id, **rover_data.dict())
+    rovers_db[rover_id] = rover
+    return rover
 
 #Delete: Deletes rover specified by ID
 #Throws 404 error if Rover not found
@@ -125,7 +148,7 @@ def deleteRover(rover_id: int):
     if rover_id not in rovers_db:
         raise HTTPException(status_code=404, detail="Rover not found")
     del rovers_db[rover_id]
-    return {"message": f"Rpver {rover_id} has been successfully deleted"}
+    return {"message": f"Rover {rover_id} has been successfully deleted"}
 
 #PUT: Send list of commands to Rover
 #404 error if Rover not found
@@ -134,12 +157,12 @@ def deleteRover(rover_id: int):
 def updateRover(rover_id: int):
     if rover_id not in rovers_db:
         raise HTTPException(status_code=404, detail="Rover not found")
-    if rovers_db[rover_id].status == 'ns':
+    elif rovers_db[rover_id].status == 'ns':
         raise HTTPException(status_code=405, detail="Rover has not started")
     elif rovers_db[rover_id].status == 'fin':
         raise HTTPException(status_code=405, detail="Rover is finished")
     # Update the rover object in the database
-    response = requests.get('https://coe892.reev.dev/lab1/rover/' + str(number))
+    response = requests.get('https://coe892.reev.dev/lab1/rover/' + str(rover_id))
     data = response.text
     parse_json = json.loads(data)
     moves = parse_json['data']['moves']
@@ -149,6 +172,13 @@ def updateRover(rover_id: int):
 
 #POST: Dispatch Rover with specified ID
 @app.post('/rovers/{rover_id}/dispatch')
-def dispatchRover():
-    #do something
-    return {'Output': 'This is a test for the Dispatch Rovers.'}
+def dispatchRover(rover_id: int):
+    if rover_id not in rovers_db:
+        raise HTTPException(status_code=404, detail="Rover not found")
+    roverMovement(rover_id)
+    return {"rover_id": rover_id}
+
+def roverMovement(rover_id: int):
+    #todo: Create function that handles movement of rover.
+    #This function will be called in dispatchRover()
+    commands = rovers_db[rover_id].data
